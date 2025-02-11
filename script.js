164a62d761e7b1b82175a730ac1317a150cd7018
Maziko66 <maziko66@gmail.com>
2025-02-12 00:16:33 +0300

    blocked right click w js

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Toggle subitems in the sidebar
+
+    const videoBackground = document.getElementById('video-background');
+    if (videoBackground) {
+        videoBackground.addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+        });
+    }
+
+
     const expandableLinks = document.querySelectorAll('.sidebar .expandable .expand-link');
     expandableLinks.forEach(link => {
         link.addEventListener('click', function (e) {
-            e.preventDefault(); // Prevent default link behavior
+            e.preventDefault(); 
             const parent = this.parentElement;
-            parent.classList.toggle('open'); // Toggle the 'open' class
+            parent.classList.toggle('open'); 
         });
     });
 
-    // Handle portfolio box display
+
     const portfolioLinks = document.querySelectorAll('.portfolio-link');
     const portfolioBoxes = document.querySelectorAll('.portfolio-box');
     const closeButtons = document.querySelectorAll('.portfolio-box .close-button');
 
     portfolioLinks.forEach(link => {
         link.addEventListener('click', function (e) {
-            e.preventDefault(); // Prevent default link behavior
-            const targetId = this.getAttribute('href').substring(1); // Get the target ID
+            e.preventDefault(); 
+            const targetId = this.getAttribute('href').substring(1); 
             portfolioBoxes.forEach(box => {
                 if (box.id === targetId) {
-                    box.classList.add('active'); // Show the target portfolio box
+                    box.classList.add('active'); 
                 } else {
-                    box.classList.remove('active'); // Hide other portfolio boxes
+                    box.classList.remove('active'); 
                 }
             });
         });
@@ -31,7 +39,11 @@ document.addEventListener('DOMContentLoaded', function () {
     closeButtons.forEach(button => {
         button.addEventListener('click', function () {
             const portfolioBox = this.closest('.portfolio-box');
-            portfolioBox.classList.remove('active'); // Hide the portfolio box
+            portfolioBox.classList.remove('active'); 
         });
+    });
+
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault(); 
     });
 });
